0b9bda41b02917d0627af00d7dcabb8ddbab841e
sukron jazuli <40122136+sukronjazuli@users.noreply.github.com>
2025-12-05 13:44:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -417,9 +417,9 @@ Jawaban (ringkas & akurat):"""
 def main():
     # st.title("📄 Analisis Verifikasi Klaim INA-CBG Edisi 2 Dengan AI")
     st.markdown("""
-        <h2 style='margin-top: -3rem; padding-top: 0;'>
+        <h1 style='margin-top: -3rem; padding-top: 0;'>
             📄 Analisis Verifikasi Klaim INA-CBG Edisi 2 Dengan AI
-        </h2>
+        </h1>
     """, unsafe_allow_html=True)
     
     setup_environment()
